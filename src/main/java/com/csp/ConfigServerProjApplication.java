cdf8fada0a71037be206217bb14727e2a4b34cde
skakbarahemad <sk.akbarahemad@gmail.com>
2024-07-08 16:22:18 +0530

    first commit

---
+++ src/main/java/com/csp/ConfigServerProjApplication.java
@@ -0,0 +1,13 @@
+package com.csp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/csp/ConfigServerProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
